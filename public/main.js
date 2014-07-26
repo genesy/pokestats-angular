49c315f072d01b26b110daafb1f96029bf79f425
Gene Sy <gnsyftw@gmail.com>
2014-07-27 06:26:20 +0800

    Adds learned moves and abilities

--- public/main.js
+++ public/main.js
@@ -65,3 +65,15 @@ app.directive('pokemon', function(){
 		}
 	};
 });
+
+app.filter('regex', function() {
+	return function(input, field, regex) {
+		var patt = new RegExp(regex);      
+		var out = [];
+		for (var i = 0; i < input.length; i++){
+			if(patt.test(input[i][field]))
+			out.push(input[i]);
+		}
+		return out;
+	};
+});
