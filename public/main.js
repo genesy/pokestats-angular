f33edf19f33deb498b922290151a66aac9220b35
Gene Sy <gnsyftw@gmail.com>
2014-07-27 06:03:10 +0800

    Adds move and ability.json Adds package.json
    Adds stats display for selected pokemon

--- public/main.js
+++ public/main.js
@@ -2,12 +2,66 @@ var app = angular.module('Pokestats', []);
 
 app.controller('PokemonCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.pokemons = '';
-	$scope.types = '';
+	$scope.moves = '';
+	$scope.ability = '';
+	$scope.types = [
+		"Normal",
+		"Fire",
+		"Water",
+		"Electric",
+		"Grass",
+		"Ice",
+		"Fighting",
+		"Poison",
+		"Ground",
+		"Flying",
+		"Psychic",
+		"Bug",
+		"Rock",
+		"Ghost",
+		"Dragon",
+		"Dark",
+		"Steel",
+		"Fairy"
+	];
+
 	$http({
 		method: 'GET',
 		url: 'data/pokemon.json'
 	}).success(function(data, status, headers, config) {
 		$scope.pokemons = data;
-		
+		$scope.selectedPokemon = $scope.pokemons[0];
+		console.log($scope.pokemons[0]);
 	})
+
+	$http({
+		method: 'GET',
+		url: 'data/move.json'
+	}).success(function(data) {
+		$scope.moves = data;
+	});
+
+	$http({
+		method: 'GET',
+		url: 'data/ability.json'
+	}).success(function(data) {
+		$scope.ability = data;
+	});
+
+	$scope.selectPokemon = function(pokemon) {
+		$scope.selectedPokemon = pokemon;
+		$scope.$apply();
+	}
+
 }])
+
+app.directive('pokemon', function(){
+	return {
+		scope: true,
+		link: function(scope, iElm, iAttrs, controller) {
+			iElm.on('click', function() {
+				scope.selectPokemon(scope.pokemon);
+			})
+		}
+	};
+});
