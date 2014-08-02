07d4563e386d398f4538abe0ce7135f3810a324f
Gene Sy <gnsyftw@gmail.com>
2014-08-02 08:07:05 +0800

    adds basic design

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,18 @@
-// var compass 	= require('gulp-compass');
+var compass 	= require('gulp-compass');
+var livereload 	= require('gulp-livereload');
 var gulp 		= require('gulp');
 var folder 		= './public/';
 
-// gulp.task('compass', function() {
-// 	gulp.src( folder + 'sass/*.scss' )
-// 	.pipe(compass({
-// 		config_file: './config.rb',
-// 		css: folder + 'css',
-// 		sass: folder + 'sass'
-// 	}))
-// 	.pipe(gulp.dest('app/assets/temp'));
-// });
+gulp.task('compass', function() {
+	gulp.src( folder + 'sass/*.scss' )
+	.pipe(compass({
+		config_file: './config.rb',
+		css: folder + 'css',
+		sass: folder + 'sass'
+	}))
+	// .pipe(gulp.dest('app/assets/temp'))
+		.pipe(livereload(1337))
+});
 
 gulp.task('watch', function() {
 	gulp.watch(folder + 'sass/**/*.scss', ['compass']);
